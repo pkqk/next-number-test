53aa51eeda59469dc8e57dc4f70e343552e3a329
Adam Sven Johnson <adam@pkqk.net>
2016-04-03 13:48:45 +0100

    Convert to generators

--- next-top-natural-number.py
+++ next-top-natural-number.py
@@ -6,35 +6,23 @@ def next_natural(num):
     digits = num_to_digits(num)
     n_sum = sum(digits)
     possible = find_sums(len(digits), n_sum)
-    possible_nums = [digits_to_num(digits) for digits in possible]
-    greater_nums = filter(lambda x: x > int(num), possible_nums)
-    if any(greater_nums):
-        next_greatest = sorted(greater_nums)[0]
-        return next_greatest
-    else:
-        return -1
-
-
-def filter_num(num):
-    def f(digits):
-        return int(''.join(digits))
-    return f
+    possible_nums = (digits_to_num(digits) for digits in possible)
+    greater_nums = (n for n in possible_nums if n > int(num))
+    for n in greater_nums:
+        return n
+    return -1
 
 
 def find_sums(digit_count, n_sum):
     if digit_count == 1:
         if n_sum < 10:
-            return [[n_sum]]
-        else:
-            return []
+            yield [n_sum]
     else:
-        sums = []
         for i in range(0, min(n_sum, 10)):
             new_sum = n_sum - i
             if new_sum > 0:
                 for new_digits in find_sums(digit_count - 1, new_sum):
-                    sums.append([i] + new_digits)
-        return sums
+                    yield [i] + new_digits
 
 
 def num_to_digits(num):
@@ -52,6 +40,10 @@ class TestNumberFinder(unittest.TestCase):
 
     def test_0200(self):
         self.assertEqual(1001, next_natural('0200'))
+
+    def test_09999999999999(self):
+        #self.assertEqual(18999999999999, next_natural('09999999999999'))
+        self.assertEqual(189999, next_natural('099999'))
 
     def test_90(self):
         self.assertEqual(-1, next_natural('99'))
